66d9c82790038828bafadfbfbec77685c01398b1
diegogil <dgg26496@hotmail.com>
2024-06-19 13:45:11 -0600

    Se terminan estilos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,18 @@
 import '../styles/TodoItem.css'
 import deleteButton from '../assets/icons/delete.svg'
+import completedIcon from '../assets/icons/plus.svg'
+import { useState } from 'react';
 
 function TodoItem({text}){
+  const [completed, setCompleted] = useState({buttonStyle:''})
+  const handleClick = () => {
+
+  }
     return (
       <li className='todo-item'>
-        <span>V</span>
+        <button className='todo-button todo-completed '>
+          <img src={completedIcon}/>
+        </button>
         <p>{text}</p>
         <button className='delete-todo'><img src={deleteButton}/></button>
       </li>
