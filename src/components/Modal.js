61c8df23ebbf1bc05b7e2762458f0f9173fa7cb7
diegogil <dgg26496@hotmail.com>
2024-08-05 17:16:33 -0600

    Encapsulamiento de mensajes de error y carga

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,15 +2,23 @@ import {useState } from 'react';
 import '../styles/Modal.css'
 
 function Modal({isOpen,newTodo}){
-    const [todoText, setTodoText] = useState('')  
-    
+    const [todoText, setTodoText] = useState('')
+    const [isValidate, setValidate] = useState(true)
+    const hadleSumit = (e) => {
+        if(todoText){
+            newTodo({text:todoText, completed:false})
+        } else{
+            setValidate(false)
+        } 
+    }
     return(
         <div className="modal__background">
             <div className="modal__create-todo">
-                <textarea onChange={(e) => setTodoText(e.target.value)}placeholder="Type your next todo"/>
+                <textarea value={todoText} onChange={(e) => setTodoText(e.target.value)}placeholder="Type your next todo"/>
+                {!isValidate && <p style={{color:"red"}}>Text is required</p>}
                 <div className='modal__buttons-container'>
                     <button onClick={() => isOpen(false)}>Cancel</button>
-                    <button onClick={()=> newTodo({text:todoText, completed:false})}>Create</button>
+                    <button onClick={hadleSumit}>Create</button>
                 </div>
             </div>
         </div>
